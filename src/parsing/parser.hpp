80f1fbd17f0d10c9d1e5ab3b5ae3c2ca83050026
Abb1x <abb1x@protonmail.com>
2021-03-23 09:02:30 -0400

    DOM: parser class

---
+++ src/parsing/parser.hpp
@@ -0,0 +1,18 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include "dom.hpp"
+
+// This is where we use the DOMNode to make a tree with the HTML elements.
+class Parser
+{
+public:
+    DOMNode *root;
+    std::map<std::string, std::map<std::string, std::string>> css;
+
+    DOMNode *parse_html(std::string code);
+
+private:
+  std::string remove_whitespace(std::string str);
+}
+
+#endif
