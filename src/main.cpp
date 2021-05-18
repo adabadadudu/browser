5ee0cafe727ba3bd3dd130043bc3dc4fb2d10a43
Abb1x <abb1x@protonmail.com>
2021-05-18 17:02:12 -0400

    added telescope parameter

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,38 @@
 int main(int argc, char *argv[])
 {
 
-    if (argc > 1 && strcmp(argv[1], "--html") == 0)
-    {
-        std::string url = "127.0.0.1";
-        int port = 8000;
-        /*if (argc > 1)
+    std::string url = "127.0.0.1";
+    int port = 8000;
+    /*if (argc > 1)
         url = argv[1];
     if (argc >= 2)
         port = std::atoi(argv[2]);*/
 
-        Http h;
+    Http h;
 
-        h.connect(url, port); // For web server testing
+    h.connect(url, port); // For web server testing
 
-        std::string response = h.entry_point(url, port);
+    std::string response = h.entry_point(url, port);
 
-        printf("%s", response.c_str());
+    printf("%s", response.c_str());
 
-        Parser parser;
+    Parser parser;
 
-        auto root = parser.parse_html(response);
-        auto css = parser.css;
+    auto root = parser.parse_html(response);
+    auto css = parser.css;
 
-        orca::Engine engine(parser.root_node, css);
+    orca::Engine engine(parser.root_node, css);
 
-        Renderer renderer(css);
+    Renderer renderer(css);
 
-        renderer.render(engine.to_layout_data());
+    renderer.render(engine.to_layout_data());
 
-        renderer.main(url + ":" + std::to_string(port));
-    }
-    else
+    renderer.main(url + ":" + std::to_string(port));
+    if (argc > 1 && strcmp(argv[1], "--telescope") == 0)
     {
         Telescope telescope;
         telescope.connect("127.0.0.1");
-	std::string response = telescope.make_request();
+        std::string response = telescope.make_request();
         TelescopeParser tel_parser;
         std::vector<TelescopeObject> objects = tel_parser.parse_star("* Hello!\n** Hello2\n*** Hello3\n");
 
